265711f4a42928ba22c02565ea1daf6f852e3858
Paul Zhang <ypzhang2@gmail.com>
2012-02-27 13:55:17 -0800

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Farcaster::Application.config.secret_token = 'aed1000f4e13cc75808846f18a28dfee445df6c18cab349e55dbbdbf7e4739b0d0c6d01b10d49a9265a313a87b3f3083c1f64c5a4efd64e37e669bfb3cdbd030'
